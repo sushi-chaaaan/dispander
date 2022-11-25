2b5bc6a95d8684c896a7f83f172f0d146ad223bf
sushi-chaaaan <sushi_code@outlook.jp>
2022-11-26 01:18:31 +0900

    embedsを返すだけの最小限の動作に変更

--- dispander/module.py
+++ dispander/module.py
@@ -81,39 +81,44 @@ async def _delete_dispand(bot: discord.Client, message: discord.Message, operato
 
 async def dispand(message):
     messages = await extract_message(message)
+    embeds = []
     for m in messages:
         sent_messages = []
 
         if m.content or m.attachments:
-            sent_message = await message.channel.send(embed=compose_embed(m))
-            sent_messages.append(sent_message)
+            embeds.append(compose_embed(m))
+            # sent_message = await message.channel.send(embed=compose_embed(m))
+            # sent_messages.append(sent_message)
         # Send the second and subsequent attachments with embed (named 'embed') respectively:
         for attachment in m.attachments[1:]:
             embed = Embed()
             embed.set_image(
                 url=attachment.proxy_url
             )
-            sent_attachment_message = await message.channel.send(embed=embed)
-            sent_messages.append(sent_attachment_message)
+            embeds.append(embed)
+            # sent_attachment_message = await message.channel.send(embed=embed)
+            # sent_messages.append(sent_attachment_message)
 
         for embed in m.embeds:
-            sent_embed_message = await message.channel.send(embed=embed)
-            sent_messages.append(sent_embed_message)
+            embeds.append(embed)
+            # sent_embed_message = await message.channel.send(embed=embed)
+            # sent_messages.append(sent_embed_message)
 
         # 一番先頭のメッセージにゴミ箱のリアクションをつける
-        main_message = sent_messages.pop(0)
-        main_embed = main_message.embeds[0]
-        await main_message.add_reaction(DELETE_REACTION_EMOJI)
-        if hasattr(main_embed.author.icon, "url"):
-            icon_url = main_embed.author.icon.url
-        else:
-            icon_url = EmptyEmbed
-        main_embed.set_author(
-            name=getattr(main_embed.author, "name", EmptyEmbed),
-            icon_url=icon_url,
-            url=make_jump_url(message, m, sent_messages)
-        )
-        await main_message.edit(embed=main_embed)
+        # main_message = sent_messages.pop(0)
+        # main_embed = main_message.embeds[0]
+        # await main_message.add_reaction(DELETE_REACTION_EMOJI)
+        # if hasattr(main_embed.author.icon, "url"):
+        #     icon_url = main_embed.author.icon.url
+        # else:
+        #     icon_url = EmptyEmbed
+        # main_embed.set_author(
+        #     name=getattr(main_embed.author, "name", EmptyEmbed),
+        #     icon_url=icon_url,
+        #     url=make_jump_url(message, m, sent_messages)
+        # )
+        # await main_message.edit(embed=main_embed)
+    return embeds[:10]
 
 
 async def extract_message(message):
