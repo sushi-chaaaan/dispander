73fca4d07eeee46a34049871832d2752554d9a5a
sushi-chaaaan <mail@sushichan.live>
2023-02-20 02:07:39 +0900

    Custom embed format

--- dispander/module.py
+++ dispander/module.py
@@ -2,7 +2,7 @@ from typing import Optional
 import os
 
 import discord
-from discord import Embed
+from discord import Colour, Embed
 from discord.ext import commands
 import re
 
@@ -80,35 +80,40 @@ async def _delete_dispand(bot: discord.Client, message: discord.Message, operato
 
 async def dispand(message):
     messages = await extract_message(message)
+    embeds = []
     for m in messages:
-        sent_messages = []
+        # sent_messages = []
 
         if m.content or m.attachments:
-            sent_message = await message.channel.send(embed=compose_embed(m))
-            sent_messages.append(sent_message)
+            embeds.append(compose_embed(m))
+            # sent_message = await message.channel.send(embed=compose_embed(m))
+            # sent_messages.append(sent_message)
         # Send the second and subsequent attachments with embed (named 'embed') respectively:
         for attachment in m.attachments[1:]:
             embed = Embed()
             embed.set_image(
                 url=attachment.proxy_url
             )
-            sent_attachment_message = await message.channel.send(embed=embed)
-            sent_messages.append(sent_attachment_message)
+            embeds.append(embed)
+            # sent_attachment_message = await message.channel.send(embed=embed)
+            # sent_messages.append(sent_attachment_message)
 
         for embed in m.embeds:
-            sent_embed_message = await message.channel.send(embed=embed)
-            sent_messages.append(sent_embed_message)
+            embeds.append(embed)
+            # sent_embed_message = await message.channel.send(embed=embed)
+            # sent_messages.append(sent_embed_message)
 
         # 一番先頭のメッセージにゴミ箱のリアクションをつける
-        main_message = sent_messages.pop(0)
-        await main_message.add_reaction(DELETE_REACTION_EMOJI)
-        main_embed = main_message.embeds[0]
-        main_embed.set_author(
-            name=getattr(main_embed.author, "name", None),
-            icon_url=getattr(main_embed.author, "icon_url", None),
-            url=make_jump_url(message, m, sent_messages)
-        )
-        await main_message.edit(embed=main_embed)
+        # main_message = sent_messages.pop(0)
+        # await main_message.add_reaction(DELETE_REACTION_EMOJI)
+        # main_embed = main_message.embeds[0]
+        # main_embed.set_author(
+        #     name=getattr(main_embed.author, "name", None),
+        #     icon_url=getattr(main_embed.author, "icon_url", None),
+        #     url=make_jump_url(message, m, sent_messages)
+        # )
+        # await main_message.edit(embed=main_embed)
+    return embeds[:10]
 
 
 async def extract_message(message):
@@ -167,16 +172,25 @@ def from_jump_url(url):
 def compose_embed(message):
     embed = Embed(
         description=message.content,
+        color=Colour(0x3498DB),
         timestamp=message.created_at,
     )
     embed.set_author(
         name=message.author.display_name,
-        icon_url=message.author.avatar or f'{discord.Asset.BASE}/embed/avatars/{discord.DefaultAvatar.red}.png',
-        url=message.jump_url
+        icon_url=message.author.display_avatar or f'{discord.Asset.BASE}/embed/avatars/{discord.DefaultAvatar.red}.png',
     )
-    embed.set_footer(
-        text=message.channel.name,
-        icon_url=message.guild.icon or f'{discord.Asset.BASE}/embed/avatars/{discord.DefaultAvatar.red}.png',
+    embed.add_field(
+        name="送信した人",
+        value=message.author.mention,
+    )
+    embed.add_field(
+        name="元のメッセージ",
+        value=f"[移動]({message.jump_url})",
+        inline=False,
+    )
+    embed.add_field(
+        name="チャンネル",
+        value=message.channel.mention or "",
     )
     if message.attachments and message.attachments[0].proxy_url:
         embed.set_image(
